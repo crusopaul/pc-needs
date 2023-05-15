00b144836549cea2ef1928104416a24b76a5610c
crusopaul <crusopaul@gmail.com>
2023-05-15 06:24:41 -0400

    1.1.0
    Add new statuses for oxy and meth

--- client/main.lua
+++ client/main.lua
@@ -1,10 +1,46 @@
-local currentSpeed = 1
-local currentAcidEffect = false
-local currentDrunkEffect = false
-local currentHighEffect = false
+local currentCaffeineEffect
+local caffeineThreadSpawn
+local currentAcidEffect
+local currentDrunkEffect
+local currentHighEffect
+local currentOxyRegenEffect
+local currentOxyEffect
+local currentMethEffect
+local methThreadSpawn
+
+RegisterNetEvent('pc-needs:client:SetCaffeineEffect', function(caffeineEffect)
+    if currentCaffeineEffect == nil or currentCaffeineEffect ~= caffeineEffect then
+        currentCaffeineEffect = caffeineEffect
+
+        if currentCaffeineEffect then
+            if not caffeineThreadSpawn then
+                caffeineThreadSpawn = true
+
+                CreateThread(function()
+                    local playerId = PlayerId()
+                    local maxStamina = GetPlayerMaxStamina(playerId)
+                    local newStamina
+ 
+                    while currentCaffeineEffect do
+                        newStamina = math.floor(maxStamina / 12 + GetPlayerStamina(playerId))
+
+                        if newStamina > maxStamina then
+                            newStamina = maxStamina
+                        end
+
+                        SetPlayerStamina(playerId, newStamina * 1.0)
+                        Wait(1000)
+                    end
+
+                    caffeineThreadSpawn = false
+                end)
+            end
+        end
+    end
+end)
 
 RegisterNetEvent('pc-needs:client:SetAcidEffect', function(acidEffect)
-    if currentAcidEffect ~= acidEffect then
+    if currentAcidEffect == nil or currentAcidEffect ~= acidEffect then
         currentAcidEffect = acidEffect
 
         if currentAcidEffect then
@@ -16,7 +52,7 @@ RegisterNetEvent('pc-needs:client:SetAcidEffect', function(acidEffect)
 end)
 
 RegisterNetEvent('pc-needs:client:SetDrunkEffect', function(drunkEffect)
-    if currentDrunkEffect ~= drunkEffect then
+    if currentDrunkEffect == nil or currentDrunkEffect ~= drunkEffect then
         currentDrunkEffect = drunkEffect
 
         if currentDrunkEffect then
@@ -36,7 +72,7 @@ RegisterNetEvent('pc-needs:client:SetDrunkOutburst', function(emote)
 end)
 
 RegisterNetEvent('pc-needs:client:SetWeedEffect', function(weedEffect)
-    if currentWeedEffect ~= weedEffect then
+    if currentWeedEffect == nil or currentWeedEffect ~= weedEffect then
         currentWeedEffect = weedEffect
 
         if currentWeedEffect then
@@ -47,9 +83,60 @@ RegisterNetEvent('pc-needs:client:SetWeedEffect', function(weedEffect)
     end
 end)
 
-RegisterNetEvent('pc-needs:client:SetWalkSpeed', function(speed)
-    if currentSpeed ~= speed then
-        currentSpeed = speed
-        SetRunSprintMultiplierForPlayer(PlayerId(), currentSpeed)
+RegisterNetEvent('pc-needs:client:SetOxyEffect', function(regenEffect, screenEffect)
+    if currentOxyRegenEffect == nil or currentOxyRegenEffect ~= regenEffect then
+        currentOxyRegenEffect = regenEffect
+
+        if currentOxyRegenEffect then
+            CreateThread(function()
+                local ped = PlayerPedId()
+                local maxHealth = 200
+
+                while (not IsPedDeadOrDying(ped, 1)) and currentOxyRegenEffect do
+                    SetEntityHealth(ped, math.min(maxHealth, math.floor(maxHealth / 128 + GetEntityHealth(ped))))
+                    Wait(1000)
+                end
+            end)
+        end
+    end
+
+    if currentOxyEffect == nil or currentOxyEffect ~= screenEffect then
+        currentOxyEffect = screenEffect
+
+        if currentOxyEffect then
+            AnimpostfxPlay('DrugsDrivingIn', 0, true)
+        else
+            AnimpostfxStop('DrugsDrivingIn')
+        end
+    end
+end)
+
+RegisterNetEvent('pc-needs:client:SetMethEffect', function(methEffect)
+    if currentMethEffect == nil or currentMethEffect ~= methEffect then
+        currentMethEffect = methEffect
+
+        if currentMethEffect then
+            AnimpostfxPlay('BikerFilter', 0, true)
+            SetRunSprintMultiplierForPlayer(PlayerId(), 1.2)
+
+            if not methThreadSpawn then
+                methThreadSpawn = true
+
+                CreateThread(function()
+                    local ped = PlayerPedId()
+                    local maxArmour = GetPlayerMaxArmour(PlayerId())
+
+                    while currentMethEffect do
+                        AddArmourToPed(ped, math.floor(maxArmour / 8))
+                        Wait(5000)
+                    end
+
+                    methThreadSpawn = false
+                end)
+            end
+        else
+            AnimpostfxStop('BikerFilter')
+            SetRunSprintMultiplierForPlayer(PlayerId(), 1.0)
+        end
     end
 end)
